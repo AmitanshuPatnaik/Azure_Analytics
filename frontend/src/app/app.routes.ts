aed6e63ced8a3d772d48d645b1f6ad6c4ff31c87
Amitanshu Patnaik <amitanshu1162@gmail.com>
2026-06-16 11:41:17 +0530

    bruv too many

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,12 +3,6 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { About } from './pages/about/about';
 import { Login } from './pages/login/login';
-
-import { Pipelines } from './pipelines/pipelines';
-import { Project1 } from './pages/project1/project1';
-import { Repos } from './pages/repos/repos';
-import { Azure } from './pages/azure/azure';
-
 import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
@@ -35,29 +29,5 @@ export const routes: Routes = [
     component: About,
     canActivate: [authGuard]
   },
-
-  {
-    path: 'project1',
-    component: Project1,
-    canActivate: [authGuard]
-  },
-
-  {
-    path: 'repos',
-    component: Repos,
-    canActivate: [authGuard]
-  },
-
-  {
-    path: 'pipelines',
-    component: Pipelines,
-    canActivate: [authGuard]
-  },
-
-  {
-    path: 'azure',
-    component: Azure,
-    canActivate: [authGuard]
-  }
 
 ];
